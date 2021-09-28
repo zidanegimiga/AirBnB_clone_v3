e3b354e83da1cccfa78c95c622b8173603eb3b7e
Zidane Gimiga <33351058+zidanegimiga@users.noreply.github.com>
2021-09-28 09:16:15 +0300

    Request http://0.0.0.0:5001/api/v1/status/: If in the status is “OK”, add the class available to the div#api_status Otherwise, remove the class available to the div#api_status

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,25 @@
+$( document ).ready(function() {
+    let amenity_id = [];
+    let amenity_name = [];
+    $('input[type="checkbox"]').change(function() {
+        if (this.checked) {
+            amenity_id.push($(this).attr('data-id'));
+            amenity_name.push($(this).attr('data-name'));
+        } else {
+            amenity_id.splice(amenity_id.indexOf($(this).attr('data-id')),1);
+            amenity_name.splice(amenity_name.indexOf($(this).attr('data-name')),1);
+        }
+        if (amenity_id.length >= 0) {
+            $("DIV.amenities H4").text(amenity_name.join(', '));
+        }
+    });
+});
+
+$.get('http://0.0.0.0:5001/api/v1/status/', function(data, textStatus)
+{
+    if (data.status == "OK"){
+	$('#api_status').addClass('available');
+    } else {
+	$('#api_status').removeClass('available');
+    }
+});
